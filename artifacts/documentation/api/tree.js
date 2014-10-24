96aea8230c5cd6bf7e4b2d478351e0c807e61e7f
Woodhouse <contact@icecave.com.au>
2014-10-24 06:38:22 +0000

    Publishing artifacts from build 1.

---
+++ artifacts/documentation/api/tree.js
@@ -0,0 +1,8 @@
+var tree = [["Icecave",,"",
+                [["Stump","Icecave\/Stump.html","",
+                [["Logger","Icecave\/Stump\/Logger.html","",
+                [        ]],["PackageInfo","Icecave\/Stump\/PackageInfo.html","",
+                [        ]],["PrefixLogger","Icecave\/Stump\/PrefixLogger.html","",
+                [        ]],["PrefixableInterface","Icecave\/Stump\/PrefixableInterface.html","",
+                [        ]]        ]]        ]]        ]
+
