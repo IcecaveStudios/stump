478f4e8d4882392d64bf726cca2d8c0d8764ed35
Woodhouse <contact@icecave.com.au>
2015-01-15 08:24:21 +0000

    Publishing artifacts from build 30.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -4,7 +4,5 @@ var tree = [["Icecave",,"",
                 [        ]],["ExceptionRendererInterface","Icecave\/Stump\/ExceptionRendererInterface.html","",
                 [        ]],["Logger","Icecave\/Stump\/Logger.html","",
                 [        ]],["PackageInfo","Icecave\/Stump\/PackageInfo.html","",
-                [        ]],["ParentLoggerInterface","Icecave\/Stump\/ParentLoggerInterface.html","",
-                [        ]],["SubLogger","Icecave\/Stump\/SubLogger.html","",
                 [        ]]        ]]        ]]        ]
 
