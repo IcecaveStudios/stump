12f1dd3f99e594674c217064a5ce651ce9b9399d
Woodhouse <contact@icecave.com.au>
2015-02-01 05:34:05 +0000

    Publishing artifacts from build 44.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -1,8 +1,13 @@
 var tree = [["Icecave",,"",
                 [["Stump","Icecave\/Stump.html","",
                 [["ExceptionRenderer","Icecave\/Stump\/ExceptionRenderer.html","",
-                [        ]],["ExceptionRendererInterface","Icecave\/Stump\/ExceptionRendererInterface.html","",
-                [        ]],["Logger","Icecave\/Stump\/Logger.html","",
+                [["ExceptionRenderer","Icecave\/Stump\/ExceptionRenderer\/ExceptionRenderer.html","",
+                [        ]],["ExceptionRendererInterface","Icecave\/Stump\/ExceptionRenderer\/ExceptionRendererInterface.html","",
+                [        ]]        ]],["MessageRenderer","Icecave\/Stump\/MessageRenderer.html","",
+                [["AnsiMessageRenderer","Icecave\/Stump\/MessageRenderer\/AnsiMessageRenderer.html","",
+                [        ]],["MessageRendererInterface","Icecave\/Stump\/MessageRenderer\/MessageRendererInterface.html","",
+                [        ]],["PlainMessageRenderer","Icecave\/Stump\/MessageRenderer\/PlainMessageRenderer.html","",
+                [        ]]        ]],["Logger","Icecave\/Stump\/Logger.html","",
                 [        ]],["PackageInfo","Icecave\/Stump\/PackageInfo.html","",
                 [        ]]        ]]        ]]        ]
 
